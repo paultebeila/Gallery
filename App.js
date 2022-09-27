c7626c624b25d643993a36d21af4ba0f3f674030
paultebeila <paultebeila@gmail.com>
2022-09-27 10:00:30 +0200

    update

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { Camera } from 'expo-camera';
+import { shareAsync } from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
+
+  let cameraRef = useRef();
+  const [hasCamPermission, setHasCamPermission] = useState();
+  const [hasMedLabPermission, setHasMedLabPermission] = useState();
+
+  useEffect(() => {
+    (async() =>{
+      const camPerm = await Camera.requestCameraPermissionsAsync();
+      const medLabPerm = await MediaLibrary.requestPermissionsAsync();
+      setHasCamPermission(camPerm.status === "Granted");
+      setHasMedLabPermission(medLabPerm.status === "Granted");
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
